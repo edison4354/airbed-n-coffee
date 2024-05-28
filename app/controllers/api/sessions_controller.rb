3bdb9476acec8e1ce19d85cc6e15363cbda14b66
Edison Li <47466335+edison4354@users.noreply.github.com>
2024-05-28 08:00:32 -0700

    Add user and session routes/controllers

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,32 @@
+class Api::SessionsController < ApplicationController
+    # before_action :require_logged_out, only: [:show, :create]
+    # before_action :require_logged_in, only: [:destroy]
+
+    def show
+        if current_user
+            render json: { user: current_user }
+        else
+            render json: { user: nil }
+        end
+    end
+
+    def create
+        email = params[:email]
+        password = params[:password]
+        @user = User.find_by_credentials(email, password)
+
+        if @user
+            login(@user)
+            render json: { user: @user }
+        else
+            render json: { errors: ['The provided credentials were invalid.'] }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        if current_user
+            logout!
+            render json: { message: 'success' }
+        end
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,37 @@
 class ApplicationController < ActionController::API
     before_action :snake_case_params
 
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        current_session_token = session[:session_token]
+        @current_user ||= User.find_by(session_token: current_session_token)
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { message: 'Unauthorized' }, status: :unauthorized 
+        end
+    end
+
+    # def require_logged_out
+    #     redirect_to users_url if logged_in?
+    # end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token! if logged_in?
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
     private
 
     def snake_case_params
