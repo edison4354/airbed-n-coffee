9c7eab63c4a866dedf972694c8c8b8d16292361a
Edison Li <47466335+edison4354@users.noreply.github.com>
2024-05-28 06:43:05 -0700

    Seeding database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    # Unnecessary if using `rails db:seed:replant`
+    User.destroy_all
+  
+    puts "Resetting primary keys..."
+    # For easy testing, so that after seeding, the first `User` has `id` of 1
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+  
+    puts "Creating users..."
+    # Create one user with an easy to remember username, email, and password:
+    User.create!(
+      first_name: 'Demo', 
+      last_name: 'User', 
+      email: 'demouser@gmail.com',
+      password: 'password'
+    )
+  
+    # More users
+    10.times do 
+      User.create!({
+        first_name: Faker::Name.unique.first_name,
+        last_name: Faker::Name.unique.last_name,
+        email: Faker::Internet.unique.email,
+        password: 'password'
+      }) 
+    end
+  
+    puts "Done!"
+end
