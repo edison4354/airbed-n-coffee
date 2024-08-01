a47b4204df83fbece2ef3a3390cd25f5ed50b205
Edison Li <47466335+edison4354@users.noreply.github.com>
2024-08-01 10:55:13 -0700

    Production Build

--- public/assets/TripCard-4c7cd250.js
+++ public/assets/TripCard-4c7cd250.js
@@ -1,4 +1,4 @@
-import{r as d,u as a,j as s,s as r}from"./index-52e2e909.js";const c=d.memo(({reservation:e})=>{const i=a(),t=l=>new Date(l+"T12:00:00Z").toLocaleDateString("en-US",{month:"short",day:"numeric"}),n=()=>{i(`/trips/${e.id}?listingId=${e.listingId}`)};return s.jsxs("div",{onClick:n,className:`
+import{r as d,u as a,j as s,s as r}from"./index-e3e467de.js";const c=d.memo(({reservation:e})=>{const i=a(),t=l=>new Date(l+"T12:00:00Z").toLocaleDateString("en-US",{month:"short",day:"numeric"}),n=()=>{i(`/trips/${e.id}?listingId=${e.listingId}`)};return s.jsxs("div",{onClick:n,className:`
                 flex 
                 w-[900px] 
                 border 
